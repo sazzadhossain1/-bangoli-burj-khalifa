3bcb70f07c4e3da9a9c53d7fbc2c16cd0dba50a0
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-04-22 18:54:52 +0600

    remove slider and add another slider from swiper slider

--- src/components/CarouselData/CarouselData.js
+++ src/components/CarouselData/CarouselData.js
@@ -1,130 +1,85 @@
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 import "./CarouselData.css";
 
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
+
 const CarouselData = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: "60px",
-  };
-
   return (
-    <div className="container  mx-auto carousel-parent-div">
-      <h1 className="branches text-center text-white ">Our Branches</h1>
-
-      <Slider className="" {...settings}>
-        <div className="carousel-div mr-10">
-          <h3 className="country">Bangladesh Branch</h3>
-          <img
-            className="carousel-img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0DFgVOq-DR90A86lDxhIT4byDMNHpZMSPjw&usqp=CAU"
-            alt=""
-          />
-          <p>
-            Location: Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Sit aut modi facilis voluptates? Quaerat, dolor sapiente soluta
-            suscipit nobis repellat.
-          </p>
+    <div className="slider-div">
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <div className="">
+          <SwiperSlide className="img-swiper">
+            <img
+              src="https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide className="img-swiper">
+            <img
+              src="https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG90ZWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide className="img-swiper">
+            <img
+              src="https://images.unsplash.com/photo-1568084680786-a84f91d1153c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide className="img-swiper">
+            <img
+              src="https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide className="img-swiper">
+            <img
+              src="https://images.unsplash.com/photo-1589923158776-cb4485d99fd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide className="img-swiper">
+            <img
+              src="https://images.unsplash.com/photo-1535827841776-24afc1e255ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjN8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide className="img-swiper">
+            <img
+              src="https://images.unsplash.com/photo-1562133567-b6a0a9c7e6eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide className="img-swiper">
+            <img
+              src="https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+          </SwiperSlide>
         </div>
-        <div className="carousel-div">
-          <h3 className="country">India Branch</h3>
-          <img
-            className="carousel-img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUPm3a4rg63_Cq4Q6kp5YmJlkqydOfAwUHlRFPLasevLCMpARRs1Y6bXl0T-KSzYhZt34&usqp=CAU"
-            alt=""
-          />
-          <p>
-            Location: Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Sit aut modi facilis voluptates? Quaerat, dolor sapiente soluta
-            suscipit nobis repellat.
-          </p>
-        </div>
-        <div className="carousel-div">
-          <h3 className="country">Nepal Branch</h3>
-          <img
-            className="carousel-img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0DFgVOq-DR90A86lDxhIT4byDMNHpZMSPjw&usqp=CAU"
-            alt=""
-          />
-          <p>
-            Location: Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Sit aut modi facilis voluptates? Quaerat, dolor sapiente soluta
-            suscipit nobis repellat.
-          </p>
-        </div>
-        <div className="carousel-div">
-          <h3 className="country">Singapoor Branch</h3>
-          <img
-            className="carousel-img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-ItqWGurZN5q6LW19Ee6OuKTMEjRD7GJyRA&usqp=CAU"
-            alt=""
-          />
-          <p>
-            Location: Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Sit aut modi facilis voluptates? Quaerat, dolor sapiente soluta
-            suscipit nobis repellat.
-          </p>
-        </div>
-        <div className="carousel-div">
-          <h3 className="country">Malaysia Branch</h3>
-          <img
-            className="carousel-img"
-            src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUPm3a4rg63_Cq4Q6kp5YmJlkqydOfAwUHlRFPLasevLCMpARRs1Y6bXl0T-KSzYhZt34&usqp=CAU"
-            alt=""
-          />
-          <p>
-            Location: Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Sit aut modi facilis voluptates? Quaerat, dolor sapiente soluta
-            suscipit nobis repellat.
-          </p>
-        </div>
-        <div className="carousel-div">
-          <h3 className="country">USA Branch</h3>
-          <img
-            className="carousel-img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs1GLXZ4g_zut-gMFe9EGYb0Mk0Gb3qeytag6FFwgWByZO9b9SjOjLwiP04dWczQeEhOI&usqp=CAU"
-            alt=""
-          />
-          <p>
-            Location: Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Sit aut modi facilis voluptates? Quaerat, dolor sapiente soluta
-            suscipit nobis repellat.
-          </p>
-        </div>
-        <div className="carousel-div">
-          <h3 className="country">Dubai Branch</h3>
-          <img
-            className="carousel-img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0DFgVOq-DR90A86lDxhIT4byDMNHpZMSPjw&usqp=CAU"
-            alt=""
-          />
-          <p>
-            Location: Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Sit aut modi facilis voluptates? Quaerat, dolor sapiente soluta
-            suscipit nobis repellat.
-          </p>
-        </div>
-        <div className="carousel-div">
-          <h3 className="country">Pakisthan Branch</h3>
-          <img
-            className="carousel-img"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs1GLXZ4g_zut-gMFe9EGYb0Mk0Gb3qeytag6FFwgWByZO9b9SjOjLwiP04dWczQeEhOI&usqp=CAU"
-            alt=""
-          />
-          <p>
-            Location: Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Sit aut modi facilis voluptates? Quaerat, dolor sapiente soluta
-            suscipit nobis repellat.
-          </p>
-        </div>
-      </Slider>
+      </Swiper>
     </div>
   );
 };
