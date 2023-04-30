a8157a6ec1323617f24231afb00ff77fb5e544f8
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-01 01:35:45 +0600

    environment variable for  firebase

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,19 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDUkviYE7KqE_Mw3cnF3MCJtbTaQaM_gIQ",
-  authDomain: "bangoli-bruse-kholifa.firebaseapp.com",
-  projectId: "bangoli-bruse-kholifa",
-  storageBucket: "bangoli-bruse-kholifa.appspot.com",
-  messagingSenderId: "967676597052",
-  appId: "1:967676597052:web:0e484f9782464920488439",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+
+  // apiKey: "AIzaSyDUkviYE7KqE_Mw3cnF3MCJtbTaQaM_gIQ",
+  // authDomain: "bangoli-bruse-kholifa.firebaseapp.com",
+  // projectId: "bangoli-bruse-kholifa",
+  // storageBucket: "bangoli-bruse-kholifa.appspot.com",
+  // messagingSenderId: "967676597052",
+  // appId: "1:967676597052:web:0e484f9782464920488439",
 };
 
 // Initialize Firebase
